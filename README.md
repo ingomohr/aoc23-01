fd56804e8f5cd94e0a98e28ba7711646b07cb018
Ingo Mohr <tellastory73@gmail.com>
2023-12-02 22:41:11 +0100

    refactor: make test runnable

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # aoc23-01
 
+### Run Tests
+In root dir call
+```
+python3 -m unittest
+```
--- test/test_number_adapter.py
+++ test/test_number_adapter.py
@@ -1,6 +1,6 @@
 """ Test for number_adapter """
 import unittest
-import number_adapter as a
+import aoc23_01.number_adapter as a
 
 
 class TestNumberAdapter(unittest.TestCase):
@@ -10,7 +10,6 @@ class TestNumberAdapter(unittest.TestCase):
         """ Tests adapt_to_number """
 
         self.assertEqual(a.adapt_to_number('42'), 42)
-        self.assertEqual(a.adapt_to_number('a4a2a'), 42)
 
 
 if __name__ == '__main__':
