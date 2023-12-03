672a93fd624994a433c4a1ad8b167f1f8d1921d6
Ingo Mohr <tellastory73@gmail.com>
2023-12-03 10:01:08 +0100

    test: rename var name for better readability

--- tests/test_number_adapter.py
+++ tests/test_number_adapter.py
@@ -1,6 +1,6 @@
 """ Test for number_adapter """
 import unittest
-import aoc23_01.number_adapter as a
+import aoc23_01.number_adapter as adapter
 
 
 class TestNumberAdapter(unittest.TestCase):
@@ -9,18 +9,18 @@ class TestNumberAdapter(unittest.TestCase):
     def test_adapt_to_number(self):
         """ Tests adapt_to_number """
 
-        self.assertEqual(a.adapt_to_number('42'), 42)
-        self.assertEqual(a.adapt_to_number('a4a3'), 43)
-        self.assertEqual(a.adapt_to_number('4a5g'), 45)
-        self.assertEqual(a.adapt_to_number('4a2hj_7g'), 47)
+        self.assertEqual(adapter.adapt_to_number('42'), 42)
+        self.assertEqual(adapter.adapt_to_number('a4a3'), 43)
+        self.assertEqual(adapter.adapt_to_number('4a5g'), 45)
+        self.assertEqual(adapter.adapt_to_number('4a2hj_7g'), 47)
 
     def test_adapt_to_number_samples(self):
         """ Tests adapt_to_number with samples from the problem description """
 
-        self.assertEqual(a.adapt_to_number('1abc2'), 12)
-        self.assertEqual(a.adapt_to_number('pqr3stu8vwx'), 38)
-        self.assertEqual(a.adapt_to_number('a1b2c3d4e5f'), 15)
-        self.assertEqual(a.adapt_to_number('treb7uchet'), 77)
+        self.assertEqual(adapter.adapt_to_number('1abc2'), 12)
+        self.assertEqual(adapter.adapt_to_number('pqr3stu8vwx'), 38)
+        self.assertEqual(adapter.adapt_to_number('a1b2c3d4e5f'), 15)
+        self.assertEqual(adapter.adapt_to_number('treb7uchet'), 77)
 
 
 if __name__ == '__main__':
